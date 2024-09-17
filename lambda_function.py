55c058417d5483cf79494fde771bd7f3bd924f9d
RohanZade <111170edu@gmail.com>
2024-09-17 08:57:50 +0530

    Added spaces for pr

--- lambda_function.py
+++ lambda_function.py
@@ -8,6 +8,9 @@ def lambda_handler(event, context):
     response = requests.get("https://www.google.com/")
     print(response.text)
 
+
+
+
     d = {'col1': [1,2], 'col2': [3,4]}
     df = pd.DataFrame(data=d)
     print(df)
